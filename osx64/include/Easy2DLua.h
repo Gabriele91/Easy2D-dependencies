f33e486a4278f2c243d6d2cc5ac866586b304d11
Gabriele Di Bari <dbgabri@gmail.com>
2015-11-08 16:54:58 +0100

    First commit

---
+++ osx64/include/Easy2DLua.h
@@ -0,0 +1,22 @@
+#ifndef EASY2DLUA_H
+#define EASY2DLUA_H
+
+#if defined(USE_LUA)  
+	#include <lua/lua.hpp>
+#elif defined(USE_LUA_JIT)
+	#error "LuaJIT not support"
+#else
+	#error "Error, must to be define USE_LUA or USE_LUA_JIT"
+#endif
+
+
+namespace Easy2D
+{
+    #include <LuaBridge/LuaBridge.h>
+    //#include <LuaBridge/RefCountedObject.h>
+    //#if defined(USE_LUABRIDGE_PTR)
+    //    #include <LuaBridge/RefCountedPtr.h>
+    //#endif
+};
+
+#endif
